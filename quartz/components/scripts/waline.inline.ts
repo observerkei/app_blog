f1121f554aebfef26e97464bac49b73c6aaece09
observerkei <observerkei@foxmail.com>
2024-03-31 06:05:55 +0800

    refactor: add load components speed of waline

--- quartz/components/scripts/waline.inline.ts
+++ quartz/components/scripts/waline.inline.ts
@@ -3,23 +3,27 @@ import Comment from '../../../content/.obsidian/plugins/waline/waline-comment.in
 import Pageview from '../../../content/.obsidian/plugins/waline/waline-pageview.inline'
 
 
-var is_404 = false
-var bodyTag = document.querySelector('body');
-var dataSlugValue = bodyTag.getAttribute('data-slug');
-if (dataSlugValue === "404") {
-  is_404 = true
+function wait_load() {
+  var is_404 = false
+  var bodyTag = document.querySelector('body');
+  var dataSlugValue = bodyTag.getAttribute('data-slug');
+  if (dataSlugValue === "404") {
+    is_404 = true
+  }
+
+  if (!is_404) {
+    try {
+      Comment();
+    } catch (err) {
+    
+    }
+  
+    try {
+      Pageview();
+    } catch (err) {
+    
+    }
+  }
 }
 
-if (!is_404) {
-  try {
-    Comment();
-  } catch (err) {
-  
-  }
-  
-  try {
-    Pageview();
-  } catch (err) {
-  
-  }
-}
+setTimeout(() => wait_load(), 100);
