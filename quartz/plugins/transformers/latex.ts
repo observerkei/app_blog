25f581467153200f0b7a4e14f8f2bd2ba06ff29f
observerkei <observerkei@foxmail.com>
2024-12-28 12:29:51 +0800

    fix: katex@0.16.11 cdn.jsdelivr.net code 200.

--- quartz/plugins/transformers/latex.ts
+++ quartz/plugins/transformers/latex.ts
@@ -8,6 +8,7 @@ import { KatexOptions } from "katex"
 import { Options as MathjaxOptions } from "rehype-mathjax/svg"
 //@ts-ignore
 import { Options as TypstOptions } from "@myriaddreamin/rehype-typst"
+import katexCSS from 'katex/dist/katex.min.css';
 
 interface Options {
   renderEngine: "katex" | "mathjax" | "typst"
@@ -49,11 +50,11 @@ export const Latex: QuartzTransformerPlugin<Partial<Options>> = (opts) => {
       switch (engine) {
         case "katex":
           return {
-            css: [{ content: "https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/katex.min.css" }],
+            css: [{ content: "static/node_modules/katex@0.16.11/dist/katex.min.css" }],
             js: [
               {
                 // fix copy behaviour: https://github.com/KaTeX/KaTeX/blob/main/contrib/copy-tex/README.md
-                src: "https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/contrib/copy-tex.min.js",
+                src: "static/node_modules/katex@0.16.11/dist/contrib/copy-tex.min.js",
                 loadTime: "afterDOMReady",
                 contentType: "external",
               },
