0da4872788e219fcc4ac6759925482951e3d46f4
observerkei <observerkei@foxmail.com>
2025-03-29 14:53:06 +0800

    fix: Graphical views are restricted to singletons

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -133,19 +133,5 @@ export const defaultListPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph({
-      localGraph: {
-        depth: 2,
-        removeTags: [
-          "excalidraw"
-        ]
-      },
-      globalGraph: {
-        removeTags: [
-          "Note",
-          "excalidraw"
-        ]
-      }
-    }),
   ],
 }
