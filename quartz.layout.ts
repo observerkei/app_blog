8ed69658d4641d40cc6d41d87c1f39010c653ded
observerkei <observerkei@foxmail.com>
2025-03-30 18:47:41 +0800

    refactor: Fixing internal links

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -132,19 +132,5 @@ export const defaultListPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph({
-      localGraph: {
-        depth: 2,
-        removeTags: [
-          "excalidraw"
-        ]
-      },
-      globalGraph: {
-        removeTags: [
-          "Note",
-          "excalidraw"
-        ]
-      }
-    })
   ],
 }
