0d3b89677804fe6ad45938b2b438aa04e737da71
observerkei <observerkei@foxmail.com>
2025-04-05 20:59:23 +0800

    style: up c..

--- quartz.config.ts
+++ quartz.config.ts
@@ -86,7 +86,7 @@ const config: QuartzConfig = {
       Plugin.Static(),
       Plugin.NotFoundPage(),
       // Comment out CustomOgImages to speed up build time
-      Plugin.CustomOgImages(),
+     // Plugin.CustomOgImages(),
     ],
   },
 }
